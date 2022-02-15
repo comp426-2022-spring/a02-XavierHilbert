b98ab2ddeb54067984db2511229ed33fd66a621e
XavierHilbert <xavierh@email.unc.edu>
2022-02-15 15:41:56 -0500

    added edge case to guess flips

--- guess-flip.js
+++ guess-flip.js
@@ -1,8 +1,8 @@
 import {flipACoin} from "/Users/xavierh/a02-XavierHilbert/modules/coin.mjs"
 import minimist from "minimist"
 const args = minimist(process.argv.slice(2))
-const guess = args["call"] || - 1
-if(guess === -1){
+const guess = args["call"]
+if(guess !== "heads" || guess !== "tails"){
     console.log("Error: no input.")
     console.log("Usage: node guess-flip --call=[heads|tails]")
 } else {
