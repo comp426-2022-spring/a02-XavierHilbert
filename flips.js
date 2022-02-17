ea1184897bf83dcbf88821d52d74d07fe29f563e
XavierHilbert <xavierh@email.unc.edu>
2022-02-17 15:43:20 -0500

    added if branch if no args given to flips

--- flips.js
+++ flips.js
@@ -1,7 +1,11 @@
-import {coinFlips, countFlips} from "./modules/coin.mjs"
+import {coinFlips, countFlips, coinFlip} from "./modules/coin.mjs"
 import minimist from "minimist"
 const args = minimist(process.argv.slice(2))
 const numOfRolls = args["number"] || 1
-let flips = coinFlips(numOfRolls)
-console.log(flips)
-console.log(countFlips(flips))
+if(numOfRolls === 1){
+    console.log(coinFlip())
+} else {
+    let flips = coinFlips(numOfRolls)
+    console.log(flips)
+    console.log(countFlips(flips))
+}
