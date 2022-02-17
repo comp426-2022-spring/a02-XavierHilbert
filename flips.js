21974d88fe6cd7f0d7b301f07481228bbbd22ba2
XavierHilbert <xavierh@email.unc.edu>
2022-02-17 15:56:49 -0500

    going back to original

--- flips.js
+++ flips.js
@@ -2,10 +2,6 @@ import {coinFlips, countFlips, coinFlip} from "./modules/coin.mjs"
 import minimist from "minimist"
 const args = minimist(process.argv.slice(2))
 const numOfRolls = args["number"] || 1
-if(numOfRolls === 1){
-    console.log(coinFlip())
-} else {
-    let flips = coinFlips(numOfRolls)
-    console.log(flips)
-    console.log(countFlips(flips))
-}
+let flips = coinFlips(numOfRolls)
+console.log(flips)
+console.log(countFlips(flips))
